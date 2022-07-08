761d78c0118e58d78b40fce2d4580906bf00648e
Darek <mr.darek01@gmail.com>
2022-07-08 20:10:39 +0300

    OpenGl glfw initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,104 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
-#include <iostream>
+#include "iostream"
 
-int main(void)
+int g_WindowSizeX = 1920/2;
+int g_WindowSizeY = 1080/2;
+
+void glfwWindowSizeCallBack(GLFWwindow* pWindow, int Width, int Height)
 {
-    GLFWwindow* window;
+	g_WindowSizeX = Width;
+	g_WindowSizeY = Height;
+	glViewport( 0, 0, g_WindowSizeX, g_WindowSizeY);
+}
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(pWindow, GL_TRUE);
+	}
+}
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+int main()
+{
+	// Initialize GLFW 
+	glfwInit();
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	// Tell GLFW what version of OpenGl we are using 
+	// In this case 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    if (!gladLoadGL())
-    {
-        std::cout << "Can't load GLAD!" << std::endl;
-        return -1;
-    }
+	// Tell GLFW that we are using CORE profile
+	// That means that we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    std::cout << "OpenGl " << GLVersion.major << "." << GLVersion.minor << std::endl;
-    
-    glClearColor(1,1,0,1);
+	
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	// Create GLFWwindow object of 800 by 800 pyxels, naming it "BattleCity"
+	GLFWwindow* pWindow = glfwCreateWindow(g_WindowSizeX, g_WindowSizeY, "BattleCity", NULL, NULL);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	// Error check, if window fails to create
+	if (!pWindow)
+	{
+		std::cout << "Failed to creat GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	// Callbacks
+	glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallBack);
+	glfwSetKeyCallback(pWindow, glfwKeyCallback);
 
-    glfwTerminate();
-    return 0;
+
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(pWindow);
+
+	// Load GLAD so it configures OpenGl
+	if (!gladLoadGL())
+	{
+		std::cout << "Can't load gladGL" << std::endl;
+	}
+
+	std::cout << "Renderer : " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "OpenGl version : " << glGetString(GL_VERSION) << std::endl;
+
+
+
+	// Specify the viewport of OpenGl in the window 
+	// In this case viewport goes from x = , y = 0, x = 800, y = 800
+	glViewport(0, 0, 800, 800);
+
+
+	// Specify the color of the background
+	glClearColor(0.07, 0.13, 0.17, 1);
+
+	// Clean the black buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Swap the black buffer with the front buffer
+	glfwSwapBuffers(pWindow);
+
+	// Main while loop
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		glClearColor(0.07, 0.13, 0.17, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(pWindow);
+
+		// Take care of all GLFW events
+		glfwPollEvents();
+
+	}
+
+
+	// Delete window before ending the program
+	glfwDestroyWindow(pWindow);
+
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+
+	return 0;
 }
